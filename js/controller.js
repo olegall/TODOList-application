81e03eaca8cd6845bb86f037b8c3bca0b4241f35
olegall <aleynikov.oleg@gmail.com>
2014-09-14 20:02:44 +0300

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,26 +1,42 @@
 
 
-	$(window).ready(function() 
-	{
+	$(window).ready(function(){
+		
 		
 		$(".input_value").click(function(event){
 			model.emptyInput(event);
 		});
 		
-
-		$(".input_value").keypress(function(event){
-			model.task.add(event);
+		
+		
+		$(".ordinary_list .input_value").keypress(function(event){
+			if (event.which == 13){		
+				model.task.add(event);
+			}
 		});
+		
 		$(document).on("click", ".item a", function(event) {
 			model.task.remove(event);
 		});
+		
 		$(document).on("mousedown", ".item", function(event) {
 			model.task.setMoved(event);
+		});	
+		
+		
+		
+		$(".priority_list .input_value").keypress(function(event){
+			if (event.which == 13){
+				model.advancedTask.add(event);
+				model.advancedTask.assignNotification($(".priority_list .input_value").val());
+			}
 		});		
 		
-
+		
+		
 		$(document).on("mousedown", ".item", function(event) {	
 			model.makeDroppable(event);
 		});
 
+		
 	});
